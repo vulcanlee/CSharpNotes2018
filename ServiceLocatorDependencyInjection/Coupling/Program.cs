0ece41ca648f2b92722286dea13e8ab792fc988b
李進興 Vulcan <vulcan@miniasp.com>
2018-04-24 14:12:55 +0800

    服務定位與相依性注入練習

---
+++ ServiceLocatorDependencyInjection/Coupling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Coupling
+{
+    public class MyService
+    {
+        public void Run()
+        {
+            Console.WriteLine("MyService 正在執行中");
+        }
+    }
+    public class Client
+    {
+        private MyService _service;
+
+        public Client()
+        {
+            // 建立 Client & MyService 類別間的耦合關係
+            Console.WriteLine("使用 new 建立一個新的 MyService 執行個體");
+            this._service = new MyService();
+        }
+        public void Start()
+        {
+            Console.WriteLine("MyService 啟動");
+            this._service.Run();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("具有緊密耦合的範例");
+            Console.WriteLine("主程式啟動與執行中");
+            new Client().Start();
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+
+        }
+    }
+}
