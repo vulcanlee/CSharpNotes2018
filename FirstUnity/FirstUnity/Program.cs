e3d0de103cc71292eff62000f661d7a53f133951
李進興 Vulcan <vulcan@miniasp.com>
2018-04-25 09:44:53 +0800

    DI 容器的使用 Unity

---
+++ FirstUnity/FirstUnity/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace FirstUnity
+{
+    public interface IMessage
+    {
+        void Write(string message);
+    }
+    public class ConsoleMessage : IMessage
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine($"訊息: {message} 已經寫入到螢幕上了");
+        }
+    }
+    public class FileMessage : IMessage
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine($"訊息: {message} 已經寫入到檔案上了");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterType<IMessage, ConsoleMessage>();
+
+            IMessage message = container.Resolve<IMessage>();
+
+            message.Write("Hi Vulcan");
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
