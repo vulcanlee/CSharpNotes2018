aacd7266030fe11f5a9ca837d0c705879ce357b4
李進興 Vulcan <vulcan@miniasp.com>
2018-04-25 10:12:39 +0800

    DI 容器的使用 : Autofac

---
+++ FirstAutofac/FirstAutofac/Program.cs
@@ -0,0 +1,46 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAutofac
+{
+    public interface IMessage
+    {
+        void Write(string message);
+    }
+    public class ConsoleMessage : IMessage
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine($"訊息: {message} 已經寫入到螢幕上了");
+        }
+    }
+    public class FileMessage : IMessage
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine($"訊息: {message} 已經寫入到檔案上了");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleMessage>().As<IMessage>();
+
+            IContainer container = builder.Build();
+
+            IMessage message = container.Resolve<IMessage>();
+
+            message.Write("Hi Vulcan");
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+
+        }
+    }
+}
