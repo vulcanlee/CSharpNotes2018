c520c4e5b68a7417fcb55a386403b20a0b6cc8ae
李進興 Vulcan <vulcan@miniasp.com>
2018-04-30 15:05:43 +0800

    相依反轉原則 DIP 之情境練習  違反 DIP 的例子   使用抽象型別解決此一問題

---
+++ DependencyInversion/Solution/DependencyInversion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    public interface IMessage { void Send(); }
+    public class Email : IMessage
+    {
+        public void Send() { }
+    }
+
+    public class Notification
+    {
+        private IMessage _Email;
+        public Notification(IMessage message)
+        {
+            _Email = message;
+        }
+        public void Notify() { _Email.Send(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
