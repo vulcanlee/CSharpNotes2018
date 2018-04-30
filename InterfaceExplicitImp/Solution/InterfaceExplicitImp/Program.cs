be8e832d5e2141ba3096cdec0b56342ae6167144
李進興 Vulcan <vulcan@miniasp.com>
2018-04-30 21:13:19 +0800

    明確介面實作練習

---
+++ InterfaceExplicitImp/Solution/InterfaceExplicitImp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExplicitImp
+{
+    interface IControl { void Paint(); }
+    interface ISurface { void Paint(); }
+    class SampleClass : IControl, ISurface
+    {
+        //public void Paint()
+        //{
+        //    Console.WriteLine("這是 SampleClass 的 Paint方法實作");
+        //}
+        void IControl.Paint()
+        {
+            Console.WriteLine("這是 SampleClass 的 IControl.Paint 方法實作");
+        }
+
+        void ISurface.Paint()
+        {
+            Console.WriteLine("這是 SampleClass 的 ISurface.Paint 方法實作");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass foo1 = new SampleClass();
+            IControl foo2 = (IControl)foo1;
+            ISurface foo3 = (ISurface)foo1;
+            //foo1.Paint();
+            foo2.Paint();
+            foo3.Paint();
+
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ InterfaceExplicitImp/Starter/InterfaceExplicitImp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExplicitImp
+{
+    interface IControl { void Paint(); }
+    interface ISurface { void Paint(); }
+    class SampleClass : IControl, ISurface
+    {
+        public void Paint()
+        {
+            Console.WriteLine("這是 SampleClass 的 Paint方法實作");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass foo1 = new SampleClass();
+            IControl foo2 = (IControl)foo1;
+            ISurface foo3 = (ISurface)foo1;
+            foo1.Paint();
+            foo2.Paint();
+            foo3.Paint();
+
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+
+        }
+    }
+}
