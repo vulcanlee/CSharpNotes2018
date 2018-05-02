284172517ff7d7ca3a53c0d1a504d5fe46cfee46
李進興 Vulcan <vulcan@miniasp.com>
2018-05-02 16:44:47 +0800

    練習情境 建構式參或屬性數需要有常數: Unity

---
+++ InjectingValue/Starter/InjectingValue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace InjectingValue
+{
+    public interface IMessage
+    {
+        void Write(string message);
+    }
+    public class ConsoleMessage : IMessage
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public double Cost { get; set; }
+
+        public void Write(string message)
+        {
+            Console.WriteLine($"Name: {Name}  Age: {Age}");
+            Console.WriteLine($"Cost: {Cost}");
+            Console.WriteLine($"訊息: {message} 已經寫入到螢幕上了");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterType<IMessage, ConsoleMessage>();
+
+            IMessage message = container.Resolve<IMessage>();
+            message.Write("Hi Vulcan");
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+
+        }
+    }
+}
