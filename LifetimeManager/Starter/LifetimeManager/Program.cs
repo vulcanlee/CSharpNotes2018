a8371c2e192172692fc7d18310be6ca3ed12f96b
李進興 Vulcan <vulcan@miniasp.com>
2018-05-02 17:32:44 +0800

    不同物件生命週期管理練習 : Unity

---
+++ LifetimeManager/Starter/LifetimeManager/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace LifetimeManager
+{
+    public interface IMessage
+    {
+        void Write(string message);
+    }
+    public class ConsoleMessage : IMessage
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine($"此物件的 HashCode {this.GetHashCode()}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterType<IMessage, ConsoleMessage>("TransientLifetimeManager");
+            container.RegisterType<IMessage, ConsoleMessage>("ContainerControlledLifetimeManager");
+
+            IMessage messageTransientLifetimeManager1 = container.Resolve<IMessage>("TransientLifetimeManager");
+            IMessage messageTransientLifetimeManager2 = container.Resolve<IMessage>("TransientLifetimeManager");
+            messageTransientLifetimeManager1.Write("Hi TransientLifetimeManager 1");
+            messageTransientLifetimeManager2.Write("Hi TransientLifetimeManager 2");
+
+            IMessage messageContainerControlledLifetimeManager1 = container.Resolve<IMessage>("ContainerControlledLifetimeManager");
+            IMessage messageContainerControlledLifetimeManager2 = container.Resolve<IMessage>("ContainerControlledLifetimeManager");
+            messageContainerControlledLifetimeManager1.Write("Hi ContainerControlledLifetimeManager 1");
+            messageContainerControlledLifetimeManager2.Write("Hi ContainerControlledLifetimeManager 2");
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+
+        }
+    }
+}
