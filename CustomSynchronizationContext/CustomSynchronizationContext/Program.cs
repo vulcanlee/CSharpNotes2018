43077d7ba8e6b6aec502aa809214bd06c8a2e957
李進興 Vulcan <vulcan@miniasp.com>
2018-06-19 23:31:21 +0800

    客製化同步內容 SynchronizationContext

--- CustomSynchronizationContext/CustomSynchronizationContext/Program.cs
+++ CustomSynchronizationContext/CustomSynchronizationContext/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace CustomSynchronisationContext
+namespace CustomSynchronizationContext
 {
     class MySynchronizationContext : SynchronizationContext
     {
--- CustomSynchronizationContext/CustomSynchronizationContext/Properties/AssemblyInfo.cs
+++ CustomSynchronizationContext/CustomSynchronizationContext/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("CustomSynchronisationContext")]
+[assembly: AssemblyTitle("CustomSynchronizationContext")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CustomSynchronisationContext")]
+[assembly: AssemblyProduct("CustomSynchronizationContext")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("d7577a88-2647-4820-971d-0e98e1301962")]
+[assembly: Guid("4c675f6e-b121-4879-940b-390bf0cfd1c7")]
 
 // 組件的版本資訊由下列四個值所組成: 
 //
