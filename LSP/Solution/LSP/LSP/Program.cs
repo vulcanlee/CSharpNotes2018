e3f4fe8339a8c52da432b0b539eb02694b035e3d
李進興 Vulcan <vulcan@miniasp.com>
2018-06-23 21:35:32 +0800

    請試著找出該 Rectangle / Square 類別，不符合 里氏替換原則 地方 請試著修正 Rectangle / Square  類別，使其符合 里氏替換原則

---
+++ LSP/Solution/LSP/LSP/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    public abstract class Shape
+    {
+        public abstract int Area();
+        public abstract string Desctiption();
+    }
+
+    public class Rectangle : Shape
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public override int Area()
+        {
+            return Height * Width;
+        }
+
+        public override string Desctiption()
+        {
+            return $"矩形 {Width} x {Height}";
+        }
+    }
+
+    public class Square : Shape
+    {
+        public int Sides;
+        public override int Area()
+        {
+            return Sides * Sides;
+        }
+
+        public override string Desctiption()
+        {
+            return $"正方形 {Sides} x {Sides}";
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("里氏替換原則 Liskov Substitution Principle");
+
+            Rectangle myRectangle = new Rectangle { Height = 2, Width = 3 };
+            var result = myRectangle.Area();
+            Console.WriteLine($"{myRectangle.Desctiption()}  = {result}");
+
+            Square mySquare = new Square { Sides = 3 };
+            result = mySquare.Area();
+            Console.WriteLine($"{mySquare.Desctiption()}  = {result}");
+
+            var shapes = new List<Shape>{
+                new Rectangle{Height=4,Width=6},
+                new Square{Sides=3}
+            };
+            var areas = new List<int>();
+            foreach (Shape shape in shapes)
+            {
+                result = shape.Area();
+                Console.WriteLine($"{shape.Desctiption()}  = {result}");
+            }
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LSP/Starter/LSP/LSP/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP
+{
+    public class Rectangle
+    {
+        public virtual int Height { get; set; }
+        public virtual int Width { get; set; }
+    }
+
+    public class Square : Rectangle
+    {
+        private int _height;
+        private int _width;
+        public override int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = value; _width = value;
+            }
+        }
+        public override int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value; _height = value;
+            }
+        }
+    }
+
+    public class AreaCalculator
+    {
+        public static int CalculateArea(Rectangle r)
+        {
+            return r.Height * r.Width;
+        }
+
+        public static int CalculateArea(Square s)
+        {
+            return s.Height * s.Height;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("里氏替換原則 Liskov Substitution Principle");
+
+            var myRectangle = new Rectangle { Height = 2, Width = 3 };
+            var result = AreaCalculator.CalculateArea(myRectangle);
+            Console.WriteLine($"矩形 {myRectangle.Width} x {myRectangle.Height} = {result}");
+
+            var mySquare = new Square { Height = 3 };
+            result = AreaCalculator.CalculateArea(mySquare);
+            Console.WriteLine($"正方形 {mySquare.Width} x {mySquare.Height} = {result}");
+
+            Rectangle newRectangle = new Square();
+            newRectangle.Height = 4;
+            newRectangle.Width = 6;
+            result = AreaCalculator.CalculateArea(newRectangle);
+            Console.WriteLine($"矩形 {newRectangle.Width} x {newRectangle.Height} = {result}");
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+
+        }
+    }
+}
