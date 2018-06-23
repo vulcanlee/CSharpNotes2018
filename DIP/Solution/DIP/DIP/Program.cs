d83e9d6e6a3d7e1a80ec8aadd77eb72bf0b4df01
李進興 Vulcan <vulcan@miniasp.com>
2018-06-24 00:47:58 +0800

    請試著找出底下程式碼，不符合 相依反轉原則 地方 請試著修正程式碼，使其符合 相依反轉原則

---
+++ DIP/Solution/DIP/DIP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+    public class SecurityService
+    {
+        private readonly ILoginService _LoginService;
+
+        public SecurityService(ILoginService loginService)
+        {
+            this._LoginService = loginService;
+        }
+        public bool LoginUser(string userName, string password)
+        {
+            return _LoginService.ValidateUser(userName, password);
+        }
+    }
+    public interface ILoginService
+    {
+        bool ValidateUser(string userName, string password);
+    }
+    public class LoginService : ILoginService
+    {
+        public bool ValidateUser(string userName, string password)
+        { throw new NotImplementedException(); }
+    }
+    public class GlobalLoginService : ILoginService
+    {
+        public bool ValidateUser(string userName, string password)
+        { throw new NotImplementedException(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DIP/Starter/DIP/DIP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP
+{
+    public class SecurityService
+    {
+        public bool LoginUser(string userName, string password)
+        {
+            LoginService service = new LoginService();
+            return service.ValidateUser(userName, password);
+        }
+    }
+    public class LoginService
+    {
+        public bool ValidateUser(string userName, string password)
+        { throw new NotImplementedException(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
