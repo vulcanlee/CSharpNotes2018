075f0f37f494f8b70a569fa7fcafc6b55aa2ca6c
李進興 Vulcan <vulcan@miniasp.com>
2018-06-23 22:25:53 +0800

    請試著找出該 IAllInOneCar 介面，不符合介面隔離原則 地方 請試著修正該 IAllInOneCar 介面，使其符合 介面隔離原則

---
+++ ISP/Solution/ISP/ISP/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    public interface IDriver
+    {
+        void StartEngine();
+        void Drive();
+        void StopEngine();
+    }
+    public interface IMechanic
+    {
+        void ChangeEngine();
+    }
+    public class Driver : IDriver
+    {
+        public void Drive()
+        { throw new NotImplementedException(); }
+
+        public void StartEngine()
+        { throw new NotImplementedException(); }
+
+        public void StopEngine()
+        { throw new NotImplementedException(); }
+    }
+    public class Mechanic : IMechanic
+    {
+        public void ChangeEngine()
+        { throw new NotImplementedException(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ISP/Starter/ISP/ISP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    public interface IAllInOneCar
+    {
+        void StartEngine();
+        void Drive();
+        void StopEngine();
+        void ChangeEngine();
+    }
+    public class Driver : IAllInOneCar
+    {
+        public void ChangeEngine()
+        { throw new NotImplementedException(); }
+
+        public void Drive()
+        { throw new NotImplementedException(); }
+
+        public void StartEngine()
+        { throw new NotImplementedException(); }
+
+        public void StopEngine()
+        { throw new NotImplementedException(); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
