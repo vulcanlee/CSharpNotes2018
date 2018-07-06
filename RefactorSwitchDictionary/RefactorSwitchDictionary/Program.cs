05ce1f69bc119913658e6a36567e55f559f8d1bd
李進興 Vulcan <vulcan@miniasp.com>
2018-07-06 13:56:04 +0800

    C# Switch 重構 Refactoring 1 : 使用資料字典

--- RefactorSwitchDictionary/RefactorSwitchDictionary/Program.cs
+++ RefactorSwitchDictionary/RefactorSwitchDictionary/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 
-namespace RefactorSwitchDelegate
+namespace RefactorSwitchDictionary
 {
     public class StringToColor
     {
@@ -31,49 +30,24 @@ namespace RefactorSwitchDelegate
 
     namespace RefactorSwitchDictionary.Refactoring
     {
-        #region 將不同顏色名稱轉換成為顏色物件的過程，設計成為不同的類別
-        public abstract class ColorTransfer
-        {
-            public abstract Color Transfer();
-        }
-        public class TransferToRed : ColorTransfer
-        {
-            public override Color Transfer()
-            {
-                return Color.FromArgb(0xFF, 0xFF, 0x00, 0x00);
-            }
-        }
-        public class TransferToGreen : ColorTransfer
-        {
-            public override Color Transfer()
-            {
-                return Color.FromArgb(0xFF, 0x80, 0x80, 0x80);
-            }
-        }
-        public class TransferToBlue : ColorTransfer
-        {
-            public override Color Transfer()
-            {
-                return Color.FromArgb(0xFF, 0x00, 0x00, 0xFF);
-            }
-        }
+        #region 將不同顏色名稱轉換方法之表示式，透過資料字典來儲存
         public class StringToColor
         {
-            Dictionary<string, ColorTransfer> _colorMaps;
+            Dictionary<string, Color> ColorMaps;
             public StringToColor()
             {
-                _colorMaps = new Dictionary<string, ColorTransfer>()
-                {
-                    {"Red", new TransferToRed() },
-                    {"Green", new TransferToGreen() },
-                    {"Blue", new TransferToBlue() },
-                };
+                ColorMaps = new Dictionary<string, Color>()
+            {
+                {"Red",  Color.FromArgb(0xFF, 0xFF, 0x00, 0x00)},
+                {"Green",  Color.FromArgb(0xFF, 0x80, 0x80, 0x80) },
+                {"Blue",  Color.FromArgb(0xFF, 0x00, 0x00, 0xFF)}
+            };
             }
             public Color Transfer(string name)
             {
-                if (!_colorMaps.ContainsKey(name))
+                if (!ColorMaps.ContainsKey(name))
                     throw new ArgumentException("不正確的顏色名稱");
-                return _colorMaps[name].Transfer();
+                return ColorMaps[name];
             }
         }
         #endregion
@@ -94,7 +68,7 @@ namespace RefactorSwitchDelegate
             Console.WriteLine("Press any key for continuing...");
             Console.ReadKey();
 
-            Console.WriteLine("使用 資料字典與多型 來重構方法，進行 Swith 需求設計");
+            Console.WriteLine("使用 資料字典 來重構方法，進行 Swith 需求設計");
 
             RefactorSwitchDictionary.Refactoring.StringToColor fooRefactoring = new RefactorSwitchDictionary.Refactoring.StringToColor();
             Color fooRefactoringColor = fooRefactoring.Transfer(MyColorName);
