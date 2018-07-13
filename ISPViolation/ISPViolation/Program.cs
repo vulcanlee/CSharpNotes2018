e35488f59ea489317dd0203decacfe34c503fb82
李進興 Vulcan <vulcan@miniasp.com>
2018-07-13 16:05:23 +0800

    一個違反 ISP Interface Segregation Principle 介面隔離原則 的範例說明

---
+++ ISPViolation/ISPViolation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISPViolation
+{
+    public interface IBusinessPrinters
+    {
+        void Scan();
+        void Print();
+        void Copy();
+        void FaxSending();
+        void FaxReceiving();
+    }
+    public class AllInOnePrinter : IBusinessPrinters
+    {
+        public void Copy() { Console.WriteLine("拷貝中"); }
+        public void FaxReceiving() { Console.WriteLine("傳真接收中"); }
+        public void FaxSending() { Console.WriteLine("傳真發送中"); }
+        public void Print() { Console.WriteLine("列印中"); }
+        public void Scan() { Console.WriteLine("掃描中"); }
+    }
+    public class SimplePrinter : IBusinessPrinters
+    {
+        public void Copy() { Console.WriteLine("錯誤!! 無此功能"); }
+        public void FaxReceiving() { Console.WriteLine("錯誤!! 無此功能"); }
+        public void FaxSending() { Console.WriteLine("錯誤!! 無此功能"); }
+        public void Print() { Console.WriteLine("列印中"); }
+        public void Scan() { Console.WriteLine("錯誤!! 無此功能"); }
+    }
+    public class CopyPrinter : IBusinessPrinters
+    {
+        public void Copy() { Console.WriteLine("拷貝中"); }
+        public void FaxReceiving() { Console.WriteLine("錯誤!! 無此功能"); }
+        public void FaxSending() { Console.WriteLine("錯誤!! 無此功能"); }
+        public void Print() { Console.WriteLine("列印中"); }
+        public void Scan() { Console.WriteLine("掃描中"); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
