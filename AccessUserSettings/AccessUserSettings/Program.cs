3c5f28c31ae310967ad836675a1afb332e871b56
李進興 Vulcan <vulcan@miniasp.com>
2018-07-17 23:17:59 +0800

    存取應用程式設定和使用者設定 Settings 的資料

---
+++ AccessUserSettings/AccessUserSettings/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessUserSettings
+{
+    public interface IMessage
+    {
+        void Send(string message);
+    }
+    public class SMSMessage : IMessage
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"簡訊已經送出 : {message}");
+        }
+    }
+    public class EMailMessage : IMessage
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"郵件已經送出 : {message}");
+        }
+    }
+    public class StaticFactory
+    {
+        public static IMessage GetMessage()
+        {
+            string fooValue = Properties.Settings.Default.IMessage;
+            Type fooType = Type.GetType(fooValue);
+            IMessage fooObject = Activator.CreateInstance(fooType) as IMessage;
+            return fooObject;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessage messageObject = StaticFactory.GetMessage();
+            messageObject.Send("我已經動態產生具體實作物件了");
+
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
