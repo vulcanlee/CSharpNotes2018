4a2272d31a1e36133f43af7124147f6d305facdc
李進興 Vulcan <vulcan@miniasp.com>
2018-08-10 22:32:51 +0800

    在 async void 方法內，無法捕捉到例外異常 Exception 的情況與解決方法

---
+++ AsyncVoid/AsyncVoid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncVoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncVoidException_Capture();
+        }
+
+        private static void AsyncVoidException_Capture()
+        {
+            try
+            {
+                ThrowExcpetionAsync();
+            }
+            catch (Exception)
+            {
+                // 這裡無法捕捉到例外異常
+                throw;
+            }
+        }
+
+        private static async void ThrowExcpetionAsync()
+        {
+            throw new Exception("Async Void Exception");
+        }
+    }
+}
