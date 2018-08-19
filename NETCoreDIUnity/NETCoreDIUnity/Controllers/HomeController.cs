503f5fa3db44a421f7115feaef06dd2a40bfec69
李進興 Vulcan <vulcan@miniasp.com>
2018-08-19 12:49:49 +0800

    在 ASP.NET Core 專案內使用 Unity 相依性注入 DI 容器

---
+++ NETCoreDIUnity/NETCoreDIUnity/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NETCoreDIUnity.Models;
+
+namespace NETCoreDIUnity.Controllers
+{
+    public class HomeController : Controller
+    {
+        IMessage _Message;
+        public HomeController(IMessage message)
+        {
+            _Message = message;
+        }
+        public IActionResult Index()
+        {
+            _Message.Send("Vulcan Lee");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NETCoreDIUnity/NETCoreDIUnity/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCoreDIUnity
+{
+    public interface IMessage
+    {
+        void Send(string message);
+    }
+
+    public class ConsoleMessage : IMessage
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"ConsoleMessage :{message}");
+        }
+    }
+    public class FileMessage : IMessage
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"FileMessage :{message}");
+        }
+    }
+}
