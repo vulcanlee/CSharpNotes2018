0e2ad02ea483488421c02dfd379e2559886f9215
李進興 Vulcan <vulcan@miniasp.com>
2018-11-14 18:55:48 +0800

    在 ASP.NET Core 專案內使用選項模式讀取設定檔與環境變數組態設定值

---
+++ OptionsPattern/OptionsPattern/MyOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OptionsPattern
+{
+    public class MyOptions
+    {
+        public string ConnectionString { get; set; }
+        public bool ActionMode { get; set; }
+         public string OS { get; set; }
+        public string MyName { get; set; }
+       public ExcetionMode ExcetionMode { get; set; }
+    }
+    public class ExcetionMode
+    {
+        public ExcetionModeType ShowType { get; set; }
+        public int Max { get; set; }
+    }
+    public enum ExcetionModeType
+    {
+        Detail,
+        None,
+        More
+    }
+}
